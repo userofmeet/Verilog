f99af43018a1ed2a1d66faec340af50a834a0aea
Meet Jain <meetjain538@gmail.com>
2024-02-10 22:28:53 +0530

    Update D  Flip-Flop.v

--- D  Flip-Flop.v
+++ D  Flip-Flop.v
@@ -12,3 +12,28 @@ else
 out <= d;
 end
 endmodule
+
+//Testbench for the D Flip Flop 
+module ddtestbench();
+  reg clk,reset;
+  reg d;
+  wire out;
+  dff dflipflop(clk,reset,d,out);
+  initial
+  begin
+    clk=1'b0;
+    forever #5 clk=~clk;
+  end
+  initial
+  begin 
+    reset=1'b0;
+    #5
+    reset=1'b1;
+    #10
+    d=1'b1;
+    #20
+    d=1'b0;
+    #10
+    d=1'b1;
+    end
+endmodule
