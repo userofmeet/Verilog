c06a29ebb098807ce1eeefa8f0824e802feec116
Meet Jain <meetjain538@gmail.com>
2024-02-11 18:50:08 +0530

    Update 4 to 1 MUX.v

--- 4 to 1 MUX.v
+++ 4 to 1 MUX.v
@@ -33,3 +33,53 @@ assign w3=i2&(s0)&(~s1);
 assign w4=i3&(s0)&(s1);
 assign out=w1 | w2 | w3 |w4;
 endmodule 
+
+//4to1 MUX using Behavioural modelling 
+module mux(i0,i1,i2,i3,s0,s1,q);
+input i0,i1,i2,i3,s0,s1;
+output reg q;
+always @(i0 or i1 or i2 or i3)
+case ({s1,s0})	
+2'b00: q<=i0;
+2'b01: q<=i1;
+2'b10: q<=i2;
+2'b11: q<=i3;
+endcase
+endmodule
+
+//Testbench
+module muxtest();
+reg i0,i1,i2,i3,s0,s1;
+wire q;
+mux test(i0,i1,i2,i3,s0,s1,q);
+initial
+begin
+    i0=1'b0;
+    forever #5 i0=~i0;
+end
+initial
+begin
+    i1=1'b0;
+    forever #10 i1=~i1;
+end
+initial 
+begin
+    i2=1'b0;
+    forever #15 i2=~i2;
+end
+initial 
+begin
+    i3=1'b0;
+    forever #20 i3=~i3;
+end
+initial
+begin
+    s0=1'b1;
+    forever #5 s0=~s0;
+end
+initial
+begin
+    s1=1'b1;
+    forever #10 s1=~s1;
+end
+endmodule 
